406e9abb969e9f0fdb03ef2b27b1489138d5cd2a
philippe <votredeveloppeur@gmail.com>
2018-04-08 01:01:06 +0000

    Modification de app.js et ajout du formulaire

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require ('express'); 
 var app = express ();
+//Implementation du moteur pug
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
 app.get ('/', function (req, res) { 
-  res.send ('Salut tout le monde!'); 
+  res.render ('form'); 
 });
 app.listen (3000, function () { 
   console.log ('Exemple d\'application écoutant sur le port 3000!'); 
