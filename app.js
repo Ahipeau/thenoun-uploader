4f2f5ebf7d7690539902b2c5abddac68571ab9e9
Samuel Guebo <samuel@e-voir.net>
2018-04-08 18:10:54 +0000

    Update app.js
    Refactor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require ('express'); 
 var app = express ();
-//Implementation du moteur pug
+
+// Implementing template engine Pug
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
@@ -8,5 +9,5 @@ app.get ('/', function (req, res) {
   res.render ('form'); 
 });
 app.listen (3000, function () { 
-  console.log ('Exemple d\'application écoutant sur le port 3000!'); 
+  console.log ('The app is running on port 3000!'); 
 });
