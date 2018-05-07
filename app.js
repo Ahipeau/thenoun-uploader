a5d6242e8f315795dbbe644dbaeef552a349b0c3
Ahipeau <salimonu07@gmail.com>
2018-05-07 16:31:42 +0000

    Premiere page test avec mustacheExpress

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var express = require ('express'); 
-var app = express ();
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var app = express();
 
-// Implementing template engine Pug
-app.set("view engine", "pug");
+// Register '.html' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+//moteur de template mustache
+
+app.set("view engine", "mustache");
 app.set("views", __dirname + "/views");
 
-app.get ('/', function (req, res) { 
-  res.render ('form'); 
+//Les Routes ou Le rend
+
+app.get('/', function(req, res) {
+    res.render('index', { test: 'Salut' });
 });
-app.listen (3000, function () { 
-  console.log ('The app is running on port 3000!'); 
+
+
+
+app.listen(3000, function() {
+    console.log('The app is running on port 3000!');
 });
